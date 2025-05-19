a6318571e2757fcfa43e95cab5fef736f733c1a7
adrian galan pacheco <agalanpa@uoc.edu>
2025-05-19 16:30:47 +0200

    Pipe commented

--- PEC6_Ej_Prac/ecommerce/src/app/pipe/default-image.pipe.ts
+++ PEC6_Ej_Prac/ecommerce/src/app/pipe/default-image.pipe.ts
@@ -6,6 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DefaultImagePipe implements PipeTransform {
 
   transform(value: string, defaultImg: string = 'assets/images/default.jpg'): string {
+    // If there's no value or empty, use default image
     if (!value || value.trim() === '') {
       return defaultImg;
     }
